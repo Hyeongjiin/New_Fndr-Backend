a84779875b5f28b072c4ae1500236d473a1e8f16
Hyeongjin <lipfolnp@gmail.com>
2024-06-09 22:30:09 +0900

    [Fix]Axios를 fetch로 수정

--- controllers/data.js
+++ controllers/data.js
@@ -98,18 +98,9 @@ exports.web3Post = async (req, res, next) => {
     for (let i = 0; i < COUNTRIES.length; i++) {
       const country = COUNTRIES[i];
       const apiUrl = `${api}?token=${token}&country=${country}&remote=true&limit=${LIMIT}`;
-      const response = await axios.get(apiUrl, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-          "Accept-Encoding": "gzip, deflate, br",
-          Accept: "application/json, text/plain, */*",
-        },
-        withCredentials: true,
-      });
-      const jobDataArray = response.data[2];
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      const jobDataArray = data[2];
       jobDataArray.forEach((jobData) => {
         if (!existingLinks.has(jobData.apply_url)) {
           const isVisaSponsored = jobData.description.includes("relocat");
